eb8917faa2e08d7d44f499729674796205927efb
muskan2532150 <rg2532150@gmail.com>
2022-11-02 23:25:58 +0530

    Added method in person class

--- person.rb
+++ person.rb
@@ -1,15 +1,28 @@
 require 'nameable.rb'
 
 class Person < Nameable 
-def initialize(name,age,parent_permission='true')
- super()
- @name=name
- @age=age
- @parent_permission=parent_permission
-end
+    attr_accessor: :name, :age
+    attr_reader: :id
+    def initialize(age,name='unknow',parent_permission='true')
+        super()
+        @id=
+        @name=name
+        @age=age
+        @parent_permission=parent_permission
+    end
+       
+    def correct_name
+           @name
+    end
 
-def correct_name
-    @name
-end
+    def can_use_services?
+        is_of_age? || @parent_permission
+    end
 
+    private
+    
+    def is_of_age?
+        @gae>=18
+    end
 end
+       
