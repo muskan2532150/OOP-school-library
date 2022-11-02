5160c4e9eb9187305c200b3b7848388c1771d659
muskan2532150 <rg2532150@gmail.com>
2022-11-02 23:52:07 +0530

    Added decorator methods

--- decorator.rb
+++ decorator.rb
@@ -1,3 +1,10 @@
 require 'nameable'
 class Decorator < Nameable
+    def initialize(nameable)
+        super()
+        @nameable=nameable
+    end
+    def correct_name
+        @nameable.correct_name
+    end
 end
