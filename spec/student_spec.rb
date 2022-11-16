dbbab8dc83021e1018181db34b42c6573717386a
muskan2532150 <rg2532150@gmail.com>
2022-11-16 21:37:32 +0530

    Updated test for student class methods

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -6,21 +6,13 @@ describe Student do
     student = Student.new("muskan",1,true,"A")
  
     it "Check for student object" do
-     expect(student.classroom).to eq("A")    end
-
-    it "Rental for person" do
-        book=double('book')
-        allow(book).to receive(:rentals){[]}
-        person.add_rental(book,"2/2/2222")
-        expect(person.rentals.length).to be(1)
+     expect(student.classroom).to eq("A")    
     end
 
     it "can use sevice method" do
       string=student.play_hooky
       expect(string).to eq('¯(ツ)/¯')
     end
-
-    it "
 end
 
 
