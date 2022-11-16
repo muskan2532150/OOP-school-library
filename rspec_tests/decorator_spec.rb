9c6aef4669c02683d24f319f0de93bdb0deb3414
muskan2532150 <rg2532150@gmail.com>
2022-11-16 21:28:37 +0530

    Added unit test for decorator class  methods

---
+++ rspec_tests/decorator_spec.rb
@@ -0,0 +1,11 @@
+require './decorator'
+
+describe Decorator do
+  it 'correct_name method ' do
+    nameable = double('muskan')
+    allow(nameable).to receive(:correct_name) { 'muskan' }
+    decorator = Decorator.new(nameable)
+    name = decorator.correct_name
+    expect(name).to eq('muskan')
+  end
+end
